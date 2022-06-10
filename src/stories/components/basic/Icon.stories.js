c5fbe9ab5c82a2681ecc0fe83f409d791dc8363d
bellasimi <gksmf6611@gmail.com>
2022-06-10 21:46:28 +0900

    refactor: storybook args 수정

--- src/stories/components/basic/Icon.stories.js
+++ src/stories/components/basic/Icon.stories.js
@@ -1,20 +1,20 @@
 import Icon from 'components/basic/Icon';
-import { ICON_TYPES } from 'utils/constants/icons';
 
 export default {
   title: 'Component/basic/Icon',
   component: Icon,
   argTypes: {
-    name: {
-      options: Object.keys(ICON_TYPES),
-      control: 'select',
-      defaultValue: 'LIKE_ICON',
+    name: { defaultValue: 'box', control: 'text' },
+    size: { defaultValue: 16, control: { type: 'range', min: 16, max: 80 } },
+    strokeWidth: {
+      defaultValue: 2,
+      control: { type: 'range', min: 2, max: 6 },
     },
-    color: { control: 'color' },
-    fontSize: { control: 'number' },
+    rotate: { defaultValue: 0, control: { type: 'range', min: 0, max: 360 } },
+    color: { defaultValue: '#222', control: 'color' },
   },
 };
 
 export const Default = (args) => {
-  return <Icon {...args}></Icon>;
+  return <Icon {...args} />;
 };

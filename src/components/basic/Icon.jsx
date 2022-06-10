3687b39cf512ce9c7ad45edf9c049ee252011b78
bellasimi <gksmf6611@gmail.com>
2022-06-11 04:16:22 +0900

    feat: Icon 컴포넌트 추가

--- src/components/basic/Icon.jsx
+++ src/components/basic/Icon.jsx
@@ -5,14 +5,7 @@ const IconWrapper = styled.i`
   display: inline-block;
 `;
 
-const Icon = ({
-  name,
-  size = 16,
-  strokeWidth = 2,
-  rotate,
-  color = '#222',
-  ...props
-}) => {
+const Icon = ({ name, size = 15, rotate, ...props }) => {
   const { [name]: iconUrl } = ICON_TYPES;
   const shapeStyle = {
     width: size,
@@ -20,27 +13,15 @@ const Icon = ({
     transform: rotate ? `rotate(${rotate}deg)` : undefined,
   };
   const iconStyle = {
-    'stroke-width': strokeWidth,
-    stroke: color,
     width: size,
     height: size,
   };
 
   return (
     <IconWrapper {...props} style={{ ...props.style, ...shapeStyle }}>
-      <img src={iconUrl} alt={name} />
+      <img src={iconUrl} alt={name} style={{ iconStyle }} />
     </IconWrapper>
   );
-
-  /*   const icon = require('feather-icons').icons[name];
-  const svg = icon ? icon.toSvg(iconStyle) : '';
-  const base64 = Buffer.from(svg, 'utf8').toString('base64'); 
-
-  return (
-    <IconWrapper {...props} style={{ ...props.style, ...shapeStyle }}>
-      <img src={`data:image/svg+xml;base64,${base64}`} alt={name} />
-    </IconWrapper>
-  );*/
 };
 
 export default Icon;

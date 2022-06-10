4abf5a0b464f9ea3f5b57c835ae463d0785fe832
bellasimi <gksmf6611@gmail.com>
2022-06-10 19:39:54 +0900

    feat: contants에 아이콘 추가

---
+++ src/utils/constants/icons.js
@@ -0,0 +1,7 @@
+import FavoriteBorderIcon from '@mui/icons-material/FavoriteBorder';
+import ChatBubbleOutlineIcon from '@mui/icons-material/ChatBubbleOutline';
+
+export const ICON_TYPES = {
+  LIKE_ICON: FavoriteBorderIcon,
+  REPLY_ICON: ChatBubbleOutlineIcon,
+};

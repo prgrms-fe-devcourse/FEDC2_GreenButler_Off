34a60fa24076f4ac4c525cdb19531006f7d4b6d4
bellasimi <gksmf6611@gmail.com>
2022-06-11 04:15:48 +0900

    feat: icons svg 파일 url 상수화

--- src/utils/constants/icons.js
+++ src/utils/constants/icons.js
@@ -1,7 +1,4 @@
-import FavoriteBorderIcon from '@mui/icons-material/FavoriteBorder';
-import ChatBubbleOutlineIcon from '@mui/icons-material/ChatBubbleOutline';
-
 export const ICON_TYPES = {
-  LIKE_ICON: FavoriteBorderIcon,
-  REPLY_ICON: ChatBubbleOutlineIcon,
+  LIKE_ICON:
+    'https://user-images.githubusercontent.com/79133602/173130035-a97d7b86-5864-403f-be38-e7fd31625ce1.svg',
 };

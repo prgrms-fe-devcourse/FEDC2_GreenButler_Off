7c268a46e0f8fc722bfa0076b3b415896e499724
bellasimi <gksmf6611@gmail.com>
2022-06-12 01:51:51 +0900

    refactor: 검색 아이콘이 있는 경우 나오도록 수정

--- src/components/basic/Input/index.jsx
+++ src/components/basic/Input/index.jsx
@@ -2,12 +2,13 @@ import { useCallback } from 'react';
 import styled from '@emotion/styled';
 import PropTypes, { number } from 'prop-types';
 import colors from 'utils/constants/colors';
+import Icon from 'components/basic/Icon';
 
 const { mainGreen, mainRed, grayLight } = colors;
 
 const InputWrapper = styled.div`
   width: ${({ width }) => (typeof width === number ? width : `${width}px`)};
-  padding: 10px 10px;
+  padding: 22px 22px;
   box-sizing: border-box;
   position: relative;
 `;
@@ -16,21 +17,28 @@ const Label = styled.label`
   display: ${({ isLabel }) => (isLabel ? 'block' : 'none')};
   font-size: 20px;
   margin: 0 0 15px 0;
-
   font-weight: 500;
   text-align: left;
   position: relative;
   left: 5px;
 `;
 
+const SearchIcon = styled(Icon)`
+  display: ${({ isIcon }) => (isIcon ? 'block' : 'none')};
+  position: absolute;
+  left: 40px;
+  bottom: 50%;
+  top: 50%;
+  margin: auto 0 auto 0;
+`;
+
 const StyledInput = styled.input`
   ${({ inputStyle }) => inputStyle};
-  padding: 0 20px;
+  padding: 0 ${({ isIcon }) => (isIcon ? '50px' : '22px')};
   width: 100%;
   position: relative;
   border: none;
   box-sizing: border-box;
-  flex-grow: 1;
   border-radius: 10px;
   border: ${({ inValid }) =>
     inValid ? `1px solid ${mainRed}` : `1px solid ${grayLight}`};
@@ -46,6 +54,7 @@ const Input = ({
   width = '100%',
   height = '70px',
   label,
+  isIcon = false,
   isLabel = false,
   inValid = false,
   required = false,
@@ -73,12 +82,14 @@ const Input = ({
         type={type}
         name={name}
         height={height}
+        isIcon={isIcon}
         required={required}
         placeholder={placeholder}
         inputStyle={InputStyle}
         onChange={handleChange}
         inValid={inValid}
       />
+      <SearchIcon isIcon={isIcon} name="LIKE_ICON"></SearchIcon>
     </InputWrapper>
   );
 };
@@ -92,11 +103,11 @@ Input.propTypes = {
   height: PropTypes.oneOfType([PropTypes.string, PropTypes.number]),
   initialValue: PropTypes.string,
   label: PropTypes.string,
+  isIcon: PropTypes.bool,
   isLabel: PropTypes.bool,
   inValid: PropTypes.bool,
   required: PropTypes.bool,
   fontSize: PropTypes.oneOfType([PropTypes.string, PropTypes.number]),
   placeholder: PropTypes.string,
   onChange: PropTypes.func,
-  style: PropTypes.object,
 };

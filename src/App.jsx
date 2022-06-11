931c221901cfac60b162fcc9f21ce63e11acae4c
gitul0515 <gitul0515@gmail.com>
2022-06-11 17:53:21 +0900

    style: ThemeProvider 추가

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,11 @@
-function App() {
-  return <div className="App"></div>;
-}
+import { ThemeProvider } from '@emotion/react';
+import theme from 'styles/theme';
+
+const App = () => {
+  return (
+    <ThemeProvider theme={theme}>
+    </ThemeProvider>
+  );
+};
 
 export default App;
---
+++ src/styles/theme.js
@@ -0,0 +1,14 @@
+export default {
+  colors: {
+    mainGreen: '#30A57D',
+    mainGreenLight: '#AFD0C4',
+    mainGreenWhite: '#E1EFEA',
+    mainRed: '#FC3B41',
+    black: '#3C3C3C',
+    grayDark: '#828282',
+    gray: '#A3A3A3',
+    grayLight: '#CCCCCC',
+    border: '#F1F1F1',
+    background: '#F8F8F8',
+  },
+};

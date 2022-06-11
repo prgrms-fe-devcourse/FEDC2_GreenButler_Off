f9f64033f727e382cd71b02086a6fe7338edf5b9
gitul0515 <gitul0515@gmail.com>
2022-06-11 18:46:42 +0900

    style: DefaultTemplate 추가

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,11 @@
 import { ThemeProvider } from '@emotion/react';
 import theme from 'styles/theme';
+import DefaultTemplate from 'template/DefaultTemplate';
 
 const App = () => {
   return (
     <ThemeProvider theme={theme}>
+      <DefaultTemplate></DefaultTemplate>
     </ThemeProvider>
   );
 };

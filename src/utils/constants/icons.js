2cb2795360cd6b61eb34b700ab4bd02f37b35fbf
jinist94 <325days@naver.com>
2022-06-12 02:55:55 +0900

    feat: Navigation 아이콘 등록

--- src/utils/constants/icons.js
+++ src/utils/constants/icons.js
@@ -1,4 +1,11 @@
 export const ICON_TYPES = {
   LIKE_ICON:
     'https://user-images.githubusercontent.com/79133602/173191749-0e7f1a03-ed89-4f06-8e83-ffc273086997.svg',
+  HOME: 'https://user-images.githubusercontent.com/81489300/173197682-b3de281e-4738-4ddd-b28d-e938b31b4a4d.svg',
+  ADD_POST:
+    'https://user-images.githubusercontent.com/81489300/173198489-ea659aad-3404-45d2-8495-90ea6ba1c967.svg',
+  SEARCH:
+    'https://user-images.githubusercontent.com/81489300/173197681-50ff3a54-09d3-4d11-9a40-427f7e335e27.svg',
+  MY_PAGE:
+    'https://user-images.githubusercontent.com/81489300/173197680-17749d89-3a4a-4463-a7c8-72bc8f1b8be2.svg',
 };

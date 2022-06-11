999c1c20c4eb30c55df26e9a94fc82268bd27c5d
jinist94 <325days@naver.com>
2022-06-12 04:39:15 +0900

    feat: Header icon 추가

--- src/utils/constants/icons.js
+++ src/utils/constants/icons.js
@@ -1,4 +1,10 @@
 export const ICON_TYPES = {
   LIKE_ICON:
     'https://user-images.githubusercontent.com/79133602/173191749-0e7f1a03-ed89-4f06-8e83-ffc273086997.svg',
+  ARROW_LEFT:
+    'https://user-images.githubusercontent.com/81489300/173201549-47a7d6cb-1864-477b-8ca5-296450b39976.svg',
+  NOTIFICATION:
+    'https://user-images.githubusercontent.com/81489300/173201548-ccbf5ac3-9093-479d-bc88-f2cdafe4e05f.svg',
+  MY_INFO:
+    'https://user-images.githubusercontent.com/81489300/173201550-4f2e0d56-ad31-4dcc-b8fd-b6e12d21fce2.svg',
 };

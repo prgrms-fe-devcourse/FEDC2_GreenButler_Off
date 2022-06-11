9036d0bd4561834e9d1236258ba269146fa445d5
bellasimi <gksmf6611@gmail.com>
2022-06-11 14:36:01 +0900

    chore: 스토리북 확장자 변경

---
+++ src/stories/components/basic/Input.stories.jsx
@@ -0,0 +1,38 @@
+import Input from 'components/basic/Input';
+import Viewport from 'components/Viewport';
+
+export default {
+  title: 'Component/basic/Input',
+  component: Input,
+  argTypes: {
+    label: {
+      defaultValue: 'Label',
+      control: 'text',
+    },
+    required: {
+      defaultValue: false,
+      control: 'boolean',
+    },
+    isButton: {
+      defaultValue: false,
+      control: 'boolean',
+    },
+    fontSize: { control: 'number' },
+    width: {
+      name: 'width',
+      defaultValue: 500,
+      control: { type: 'range', min: 200, max: 500 },
+    },
+    height: {
+      name: 'height',
+      defaultValue: 40,
+      control: { type: 'range', min: 20, max: 50 },
+    },
+  },
+};
+
+export const Default = (args) => (
+  <Viewport>
+    <Input {...args} />
+  </Viewport>
+);

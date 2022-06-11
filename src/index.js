7bf2cb66b964509c62c25fe08f7eb53b39e41f0e
gitul0515 <gitul0515@gmail.com>
2022-06-11 16:29:40 +0900

    style: css 초기화

--- src/index.js
+++ src/index.js
@@ -2,6 +2,8 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import './styles/reset.css';
+import './styles/index.css';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(

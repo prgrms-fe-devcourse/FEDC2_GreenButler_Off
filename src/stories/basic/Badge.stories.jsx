fbe4aaa28de1b70e86811feba9e434042d41b813
jinist94 <325days@naver.com>
2022-06-12 05:38:44 +0900

    feat(Badge): story에 예시로 보여줄 컴포넌트 변경

--- src/stories/basic/Badge.stories.jsx
+++ src/stories/basic/Badge.stories.jsx
@@ -1,5 +1,5 @@
-import Image from 'components/basic/Image';
 import Badge from 'components/basic/Badge'
+import Icon from 'components/basic/Icon';
 
 export default {
 title: 'Component/Badge',
@@ -15,7 +15,7 @@ argTypes: {
 export const Default = (args) => {
   return(
     <Badge {...args}>
-      <Image src="https://via.placeholder.com/200"/>
+      <Icon name="LIKE_ICON" size={30}/>
     </Badge>
   )
 };

457411731d9c10e0660840fa9dc392e6db7d643c
gitul0515 <gitul0515@gmail.com>
2022-06-13 16:00:34 +0900

    style: DefaultTemplate에 배경 색깔 수정

--- src/template/DefaultTemplate.jsx
+++ src/template/DefaultTemplate.jsx
@@ -32,7 +32,7 @@ const Container = styled.div`
   overflow-x: hidden;
   margin: 0 auto;
   height: ${({ height }) => `${height}`}px;
-  background-color: ${({ theme }) => theme.color.backgroundGray};
+  background-color: ${({ theme }) => theme.color.backgroundLight};
 `;
 
 export default DefaultTemplate;

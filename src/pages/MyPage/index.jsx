17e1399088365e0fc6ed8f18db79b2913bf4bfcb
bellasimi <gksmf6611@gmail.com>
2022-06-15 02:10:50 +0900

    feat: axios handler 함수들 추가

--- src/pages/MyPage/index.jsx
+++ src/pages/MyPage/index.jsx
@@ -1,7 +1,5 @@
-import { Link } from 'react-router-dom';
 import { useEffect, useState, useCallback } from 'react';
 import styled from '@emotion/styled';
-import Image from 'components/basic/Image';
 import Avatar from 'components/basic/Avatar';
 import Text from 'components/basic/Text';
 import theme from 'styles/theme';
@@ -9,8 +7,21 @@ import Icon from 'components/basic/Icon';
 import { dummyPosts, me } from 'dummy';
 import { useUserContext } from 'contexts/UserContext';
 import { getUserPosts } from 'utils/apis/postApi';
-
+import ImageContainer from './ImageContainer';
+import { useNavigate } from 'react-router-dom';
+import {
+  fullNameStyle,
+  smallTextStyle,
+  UserContainter,
+  Header,
+  UserInfo,
+  UserDetailWrapper,
+  UserDetail,
+  Tab,
+  Bottom,
+} from './style';
 const MyPage = () => {
+  const navigate = useNavigate();
   //const { currentUser } = useUserContext();
   const currentUser = me;
   const [posts, setPosts] = useState([]);
@@ -27,22 +38,6 @@ const MyPage = () => {
     handleGetUserPosts();
   }, []);
 
-  const smallTextStyle = {
-    fontSize: 16,
-    color: theme.color.fontNormal,
-  };
-
-  const nickNameStyle = {
-    display: 'block',
-    marginTop: 5,
-    fontWeight: 500,
-    fontSize: 24,
-    lineHeight: '34.75px',
-    cursor: 'pointer',
-  };
-
-  const dummyPlace = 'https://via.placeholder.com/200'; //추후 스켈레톤으로 교체
-
   return (
     <UserContainter>
       <Header />
@@ -57,24 +52,26 @@ const MyPage = () => {
             `https://user-images.githubusercontent.com/79133602/173279398-ac52268b-082f-4fd2-8748-b60dad85b069.png`
           }
         />
-        <Text
-          style={{
-            ...nickNameStyle,
-          }}
-        >
-          {currentUser.fullName}
-        </Text>
+        <Text style={{ ...fullNameStyle }}>{currentUser.fullName}</Text>
         <UserDetailWrapper>
           <UserDetail>
-            <Text style={{ ...smallTextStyle }}> 게시물</Text>
+            <Text style={{ ...smallTextStyle }} onClick={() => setPosts(userPosts)}>
+              {' '}
+              게시물
+            </Text>
             <Text fontSize={18}> {currentUser.posts.length}</Text>
           </UserDetail>
           <UserDetail>
-            <Text style={{ ...smallTextStyle }}>팔로워</Text>
+            <Text style={{ ...smallTextStyle }} onClick={() => navigate('/user/follow')}>
+              팔로워
+            </Text>
             <Text fontSize={18}> {currentUser.followers.length}</Text>
           </UserDetail>
           <UserDetail>
-            <Text style={{ ...smallTextStyle }}> 팔로잉</Text>
+            <Text style={{ ...smallTextStyle }} onClick={() => navigate('/user/follow')}>
+              {' '}
+              팔로잉
+            </Text>
             <Text fontSize={18}> {currentUser.following.length}</Text>
           </UserDetail>
         </UserDetailWrapper>
@@ -101,121 +98,12 @@ const MyPage = () => {
 
       <Tab>
         <Icon name="LIKE_ICON" size={18} onClick={() => setPosts(userPosts)} />
-        <Icon
-          name="LIKE_ICON"
-          size={18}
-          onClick={() => setPosts(uesrLikePosts)}
-        />
+        <Icon name="LIKE_ICON" size={18} onClick={() => setPosts(uesrLikePosts)} />
       </Tab>
-      <ImageContainer>
-        {posts.map((post) => (
-          <Link to={`/post/detail/${post._id}`} key={Math.random()}>
-            <Image
-              style={{
-                overflow: 'hidden',
-                cursor: 'pointer',
-              }}
-              width="100%"
-              block={true}
-              threshold={0.1}
-              className="post-item"
-              src={
-                post.image ||
-                `https://user-images.githubusercontent.com/79133602/173282447-b5cdf98e-4372-4284-9795-b824acf2283d.png`
-              }
-              alt={posts._id}
-              placeholder={dummyPlace}
-            />
-          </Link>
-        ))}
-      </ImageContainer>
+      <ImageContainer posts={posts} />
       <Bottom />
     </UserContainter>
   );
 };
 
 export default MyPage;
-
-const UserContainter = styled.div`
-  width: 100%;
-  position: relative;
-  background-color: white;
-`;
-
-const Header = styled.div`
-  height: 90px;
-  width: 100%;
-  position: fixed;
-  top: 0;
-  left: 0;
-  right: 0;
-  border-bottom: 1px solid ${theme.color.gray};
-  background-color: white;
-  z-index: 2;
-  display: flex;
-  justify-content: space-between;
-`;
-
-const Bottom = styled.div`
-  height: 90px;
-  width: 100%;
-  border-top: 1px solid ${theme.color.gray};
-  position: fixed;
-  bottom: 0;
-  left: 0;
-  right: 0;
-  background-color: white;
-  display: flex;
-  justify-content: space-between;
-  align-items: center;
-`;
-
-const Tab = styled.div`
-  height: 50px;
-  width: 100%;
-  display: flex;
-  align-items: flex-end;
-  padding-bottom: 10px;
-  margin-top: 15px;
-  justify-content: space-around;
-  border-bottom: 1px solid ${theme.color.gray};
-  i {
-    cursor: pointer;
-  }
-`;
-
-const ImageContainer = styled.div`
-  width: 100%;
-  display: grid;
-  grid-template-columns: repeat(3, 30%);
-  gap: 10px;
-  justify-content: center;
-  margin-top: 10px;
-`;
-
-const UserInfo = styled.div`
-  text-align: center;
-  margin: 120px auto 0 auto;
-  position: relative;
-`;
-
-const UserDetailWrapper = styled.div`
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  gap: 20px;
-  margin: 20px 0 0 0;
-  cursor: pointer;
-`;
-
-const UserDetail = styled.div`
-  display: flex;
-  flex-direction: column;
-  justify-content: center;
-  gap: 10px;
-
-  > div:first-of-type {
-    font-size: 16px;
-    color: ${theme.color.fontNormal};
-  }
-`;
--- src/pages/UserPage/index.jsx
+++ src/pages/UserPage/index.jsx
@@ -1,48 +1,84 @@
-import { Link } from 'react-router-dom';
 import { useEffect, useState, useCallback } from 'react';
-import styled from '@emotion/styled';
-import Image from 'components/basic/Image';
 import Avatar from 'components/basic/Avatar';
 import Text from 'components/basic/Text';
 import Button from 'components/basic/Button';
-import { other, dummyPosts } from 'dummy';
+import { me, other, dummyPosts, users } from 'dummy';
 import theme from 'styles/theme';
 import Icon from 'components/basic/Icon';
 import { useParams } from 'react-router-dom';
-import { getUserPosts } from 'utils/apis/postApi';
+import { useUserContext } from 'contexts/UserContext';
+import { initialUserData } from 'contexts/UserContext/reducer';
+import { getUser, Follow, unFollow } from 'utils/apis/userApi';
+import { getUserPosts, getPostData } from 'utils/apis/postApi';
+import ImageContainer from './ImageContainer';
+import { useNavigate } from 'react-router-dom';
+import {
+  followButtonStyle,
+  followingButtonStyle,
+  fullNameStyle,
+  smallTextStyle,
+  UserContainter,
+  Header,
+  UserInfo,
+  UserDetailWrapper,
+  UserDetail,
+  Tab,
+  Bottom,
+} from './style';
 
 const UserPage = () => {
-  const userId = useParams();
-  const user = other;
+  const navigate = useNavigate();
+  //const user = other;
+  const currentUser = me;
+  const pageUserId = '62a0ab92703fdd3a82b4e73f';
+
+  const { onFollow, onUnfollow } = useUserContext();
+  //const { currentUser, onFollow, onUnfollow  } = useUserContext();
+  //const pageUserId = useParams();
+
+  const [user, setUser] = useState(initialUserData.currentUser);
   const [posts, setPosts] = useState([]);
   const [userPosts, setUserPosts] = useState([]);
-  const [uesrLikePosts, setUserLikePosts] = useState(dummyPosts);
+  const [uesrLikePosts, setUserLikePosts] = useState([]);
+
+  useEffect(() => {
+    handleGetUser();
+    handleGetUserPosts();
+    handleGetLikePosts();
+  }, []);
+
+  const handleGetUser = useCallback(async () => {
+    const { data } = await getUser('62a0ab92703fdd3a82b4e73f');
+    //const { data } = await getUser(pageUserId);
+    setUser(data);
+  }, []);
+
+  const likePostIds = currentUser.likes;
+  const isFollwing = currentUser.following.some((id) => id === pageUserId);
+  const [isFollow, setIsFollow] = useState(isFollwing);
 
   const handleGetUserPosts = useCallback(async () => {
-    const { data } = await getUserPosts('629e29bd6d18b41c5b238ba2');
-    //const { data } = await getUserPosts(userId);
+    const { data } = await getUserPosts('62a0ab92703fdd3a82b4e73f');
+    //const { data } = await getUserPosts(pageUserId);
     setUserPosts(data);
     setPosts(data);
   }, []);
 
-  useEffect(() => {
-    handleGetUserPosts();
+  const handleGetLikePosts = useCallback(async () => {
+    const data = await Promise.all(
+      likePostIds.map((likePostId) => getPostData(likePostId).then((result) => result.data)),
+    );
+    setUserLikePosts(data);
   }, []);
 
-  const smallTextStyle = {
-    fontSize: 16,
-    color: theme.color.fontNormal,
-  };
-
-  const followButtonStyle = {
-    width: 125,
-    height: 38,
-    color: 'white',
-    backgroundColor: theme.color.mainGreen,
-    borderRadius: 10,
-    marginTop: '25px',
-    fontSize: 16,
-  };
+  const handleFollowButton = useCallback(() => {
+    setIsFollow((isFollow) => !isFollow);
+    if (isFollow) {
+      onFollow({ userId: currentUser._id, followId: pageUserId });
+    } else {
+      onUnfollow({ unfollowId: pageUserId });
+    }
+  }, []);
 
   return (
     <UserContainter>
@@ -58,35 +94,35 @@ const UserPage = () => {
             'https://user-images.githubusercontent.com/79133602/173309944-eb758bba-9df4-4b11-a580-b62324c22ef6.png'
           }
         />
-
-        <Text
-          style={{
-            display: 'block',
-            marginTop: 5,
-            fontWeight: 500,
-            fontSize: 24,
-            lineHeight: '34.75px',
-            cursor: 'pointer',
-          }}
-        >
-          {user.fullName}
-        </Text>
+        <Text style={fullNameStyle}>{user.fullName}</Text>
         <UserDetailWrapper>
           <UserDetail>
-            <Text style={{ ...smallTextStyle }}> 게시물</Text>
+            <Text style={{ ...smallTextStyle }} onClick={() => setPosts(userPosts)}>
+              {' '}
+              게시물
+            </Text>
             <Text fontSize={18}> {user.posts.length}</Text>
           </UserDetail>
           <UserDetail>
-            <Text style={{ ...smallTextStyle }}>팔로워</Text>
+            <Text style={{ ...smallTextStyle }} onClick={() => navigate('/user/follow')}>
+              팔로워
+            </Text>
             <Text fontSize={18}> {user.followers.length}</Text>
           </UserDetail>
           <UserDetail>
-            <Text style={{ ...smallTextStyle }}> 팔로잉</Text>
+            <Text style={{ ...smallTextStyle }} onClick={() => navigate('/user/follow')}>
+              {' '}
+              팔로잉
+            </Text>
             <Text fontSize={18}> {user.following.length}</Text>
           </UserDetail>
         </UserDetailWrapper>
-        <Button width="100%" style={{ ...followButtonStyle }}>
-          팔로우
+        <Button
+          width="100%"
+          style={isFollow ? { ...followingButtonStyle } : { ...followButtonStyle }}
+          onClick={handleFollowButton}
+        >
+          {isFollow ? '팔로잉' : '팔로우'}
         </Button>
       </UserInfo>
 
@@ -111,125 +147,12 @@ const UserPage = () => {
 
       <Tab>
         <Icon name="LIKE_ICON" size={18} onClick={() => setPosts(userPosts)} />
-        <Icon
-          name="LIKE_ICON"
-          size={18}
-          onClick={() => setPosts(uesrLikePosts)}
-        />
+        <Icon name="LIKE_ICON" size={18} onClick={() => setPosts(uesrLikePosts)} />
       </Tab>
-      <PostImageList>
-        {posts.map((post) => (
-          <Link to={`/post/detail/${post._id}`} key={Math.random()}>
-            <Image
-              style={{
-                overflow: 'hidden',
-                cursor: 'pointer',
-              }}
-              width="100%"
-              height="100%"
-              className="post-item"
-              src={
-                post.image ||
-                `https://user-images.githubusercontent.com/79133602/173282447-b5cdf98e-4372-4284-9795-b824acf2283d.png`
-              }
-            />
-          </Link>
-        ))}
-      </PostImageList>
+      <ImageContainer posts={posts} />
       <Bottom />
     </UserContainter>
   );
 };
 
 export default UserPage;
-
-const UserContainter = styled.div`
-  width: 100%;
-  position: relative;
-  background-color: white;
-`;
-
-const Header = styled.div`
-  height: 90px;
-  width: 100%;
-  position: fixed;
-  top: 0;
-  left: 0;
-  right: 0;
-  border-bottom: 1px solid ${theme.color.gray};
-  background-color: white;
-  z-index: 2;
-  display: flex;
-  justify-content: space-between;
-
-  i {
-    cursor: pointer;
-  }
-`;
-const Bottom = styled.div`
-  height: 90px;
-  width: 100%;
-  border-top: 1px solid ${theme.color.gray};
-  position: fixed;
-  bottom: 0;
-  left: 0;
-  right: 0;
-  background-color: white;
-  display: flex;
-  justify-content: space-between;
-  align-items: center;
-
-  i {
-    cursor: pointer;
-  }
-`;
-
-const Tab = styled.div`
-  height: 50px;
-  width: 100%;
-  display: flex;
-  align-items: flex-end;
-  padding-bottom: 10px;
-  margin-top: 15px;
-  justify-content: space-around;
-  border-bottom: 1px solid ${theme.color.gray};
-  i {
-    cursor: pointer;
-  }
-`;
-
-const PostImageList = styled.div`
-  width: 100%;
-  display: grid;
-  grid-template-columns: repeat(3, 30%);
-  gap: 10px;
-  justify-content: center;
-  margin-top: 10px;
-`;
-
-const UserInfo = styled.div`
-  text-align: center;
-  margin: 120px auto 0 auto;
-  position: relative;
-`;
-
-const UserDetailWrapper = styled.div`
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  gap: 20px;
-  margin: 20px 0 0 0;
-  cursor: pointer;
-`;
-
-const UserDetail = styled.div`
-  display: flex;
-  flex-direction: column;
-  justify-content: center;
-  gap: 10px;
-
-  > div:first-of-type {
-    font-size: 16px;
-    color: ${theme.color.fontNormal};
-  }
-`;

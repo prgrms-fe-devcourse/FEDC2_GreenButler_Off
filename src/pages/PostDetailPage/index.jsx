deeb8206f39ab6f8abe96d308f1bf94a3cc460a6
gitul0515 <gitul0515@gmail.com>
2022-06-14 16:43:39 +0900

    style: PostDetailPage 마크업 추가

--- src/pages/PostDetailPage/index.jsx
+++ src/pages/PostDetailPage/index.jsx
@@ -1,5 +1,18 @@
+import styled from '@emotion/styled';
+import Header from './Header';
+import PostDetail from './PostDetail';
+
 const PostDetailPage = () => {
-  return <div>PostDetailPage</div>;
+  return (
+    <Container>
+      <Header />
+      <PostDetail />
+    </Container>
+  );
 };
+
+const Container = styled.div`
+  min-height: 100vh;
+`;
 
 export default PostDetailPage;

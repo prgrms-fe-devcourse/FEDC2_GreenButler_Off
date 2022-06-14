7b3cc9cb37b955d43802e4a1ee9d7311a52c983a
jinist94 <325days@naver.com>
2022-06-15 06:00:05 +0900

    feat(Navigation): DefaultTemplate에 적용

--- src/components/Navigation/index.jsx
+++ src/components/Navigation/index.jsx
@@ -1,33 +1,87 @@
-import styled from "@emotion/styled"
-import Icon from "components/basic/Icon"
+import styled from '@emotion/styled';
+import Icon from 'components/basic/Icon';
+import theme from 'styles/theme';
+import { Link } from 'react-router-dom';
 
+import { useParams, useLocation } from 'react-router-dom';
+
+const { mainWhite, borderLight } = theme.color;
 const NavContainer = styled.div`
+  position: fixed;
   width: 100%;
-  position: absolute;
+  max-width: 500px;
+  margin: 0 auto;
+  left: 0;
+  right: 0;
   bottom: 0;
-  border-top: 1px solid ${({theme}) => (theme.color.borderGray)};
-`
+  border-top: 1px solid ${borderLight};
+  background-color: ${mainWhite};
+  z-index: 100;
+`;
+
 const NavList = styled.ul`
   display: flex;
-`
+
+  a {
+    display: block;
+    width: 100%;
+    padding: 30px 0;
+  }
+`;
 
 const NavItem = styled.li`
-  padding: 30px 0;
   width: 25%;
   text-align: center;
   cursor: pointer;
-`
+`;
+
 export const Navigation = () => {
+  const { pathname } = useLocation();
+
+  const showTab = ['/', '/user/mypage', '/search', '/post/detail'];
+
+  if (!showTab.includes(pathname)) {
+    return;
+  }
+
   return (
     <NavContainer>
       <NavList>
-        <NavItem><Icon name='HOME' size={27}/></NavItem>
-        <NavItem><Icon name='ADD_POST' size={27}/></NavItem>
-        <NavItem><Icon name='SEARCH' size={27}/></NavItem>
-        <NavItem><Icon name='MY_PAGE' size={27}/></NavItem>
+        <NavItem>
+          <Link to="/">
+            {pathname === '/' ? (
+              <Icon name="HOME_ACTIVE" size={27} />
+            ) : (
+              <Icon name="HOME" size={27} />
+            )}
+          </Link>
+        </NavItem>
+        <NavItem>
+          <Link to="/post/create">
+            <Icon name="ADD_POST" size={27} />
+          </Link>
+        </NavItem>
+        <NavItem>
+          <Link to="/search">
+            {pathname === '/search' ? (
+              <Icon name="SEARCH_ACTIVE" size={27} />
+            ) : (
+              <Icon name="SEARCH" size={27} />
+            )}
+          </Link>
+        </NavItem>
+        <NavItem>
+          <Link to="/user/mypage">
+            {pathname === '/user/mypage' ? (
+              <Icon name="MY_PAGE_ACTIVE" size={27} />
+            ) : (
+              <Icon name="MY_PAGE" size={27} />
+            )}
+          </Link>
+        </NavItem>
       </NavList>
     </NavContainer>
-  )
-}
+  );
+};
 
-export default Navigation
+export default Navigation;

669d3c0c0f4f57f259584c351306fe2c9645936f
jinist94 <325days@naver.com>
2022-06-15 03:18:01 +0900

    refactor(useValidInput): resetValue 추가

--- src/hooks/useValidInput.js
+++ src/hooks/useValidInput.js
@@ -2,33 +2,38 @@ import { useState } from 'react';
 
 const useValidInput = (max = 0) => {
   const [error, setError] = useState('');
-  const [inputValue, setInputValue] = useState('');
+  const [value, setValue] = useState('');
 
   const handleChange = (e) => {
-    const value = e.target.value;
+    const inputValue = e.target.value;
 
     // eslint-disable-next-line
     const reg = /[`~!@#$%^&*()_|+\-=?;:'",.<>\s\{\}\[\]\\\/]/gi;
 
-    if (max && inputValue.length > max) {
+    if (max && value.length >= max) {
       setError(`* 1-${max}글자 사이로 입력해주세요!`);
-      setInputValue(value.slice(0, max + 1));
+      setValue(inputValue.slice(0, max));
       return;
     }
-    if (reg.test(value)) {
+    if (reg.test(inputValue)) {
       setError('* 특수문자 및 띄어쓰기는 사용할 수 없습니다.');
-      setInputValue(value.replace(reg, ''));
+      setValue(inputValue.replace(reg, ''));
       return;
     }
 
-    setInputValue(value);
+    setValue(inputValue);
     setError('');
   };
 
+  const resetValue = () => {
+    setValue('');
+  };
+
   return {
-    inputValue,
+    value,
     error,
     setError,
+    resetValue,
     handleChange,
   };
 };

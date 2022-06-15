529a4eca4292bbb0bf7a44626d964398b853d1c6
bellasimi <gksmf6611@gmail.com>
2022-06-15 18:45:33 +0900

    refactor: Mypage currentUser의 likes 배열로 postId 가져오도록 변경

--- src/pages/MyPage/index.jsx
+++ src/pages/MyPage/index.jsx
@@ -2,10 +2,9 @@ import { useEffect, useState, useCallback } from 'react';
 import Avatar from 'components/basic/Avatar';
 import Text from 'components/basic/Text';
 import Icon from 'components/basic/Icon';
-import { dummyPosts, me } from 'dummy';
 import { useUserContext } from 'contexts/UserContext';
-import { getUserPosts } from 'utils/apis/postApi';
-import ImageContainer from './ImageContainer';
+import { getUserPosts, getPostData } from 'utils/apis/postApi';
+import PostImageContainer from 'components/PostImageContainer';
 import { useNavigate } from 'react-router-dom';
 import {
   fullNameStyle,
@@ -18,28 +17,45 @@ import {
   Tab,
   Bottom,
 } from './style';
+
 const MyPage = () => {
   const navigate = useNavigate();
-  //const { currentUser } = useUserContext();
-  const currentUser = me;
+  const { currentUser } = useUserContext();
   const [posts, setPosts] = useState([]);
   const [userPosts, setUserPosts] = useState([]);
-  const [uesrLikePosts, setUserLikePosts] = useState(dummyPosts);
-
-  const handleGetUserPosts = useCallback(async () => {
-    const { data } = await getUserPosts('629e29bd6d18b41c5b238ba2');
-    setUserPosts(data);
-    setPosts(data);
-  }, []);
+  const [uesrLikePosts, setUserLikePosts] = useState([]);
 
   useEffect(() => {
     handleGetUserPosts();
+    handleGetLikePosts();
+  }, []);
+
+  const handleGetUserPosts = useCallback(async () => {
+    const { id } = currentUser;
+    if (id) {
+      const { data } = await getUserPosts(id);
+      setUserPosts(data);
+      setPosts(data);
+    }
+  }, []);
+
+  const handleGetLikePosts = useCallback(async () => {
+    const { likes } = currentUser;
+    if (likes.length !== 0) {
+      const data = await Promise.all(
+        likes.map((like) => getPostData(like.post).then((result) => result.data)),
+      );
+      setUserLikePosts(data);
+    }
   }, []);
 
   return (
     <UserContainter>
       <Header />
       <UserInfo>
+        {/* 
+        //TODO: 프로필 기본 이미지를 변경할 예정
+      */}
         <Avatar
           size={136}
           style={{
@@ -59,46 +75,28 @@ const MyPage = () => {
             </Text>
             <Text fontSize={18}> {currentUser.posts.length}</Text>
           </UserDetail>
-          <UserDetail>
-            <Text style={{ ...smallTextStyle }} onClick={() => navigate('/user/follow')}>
-              팔로워
-            </Text>
+          <UserDetail onClick={() => navigate('/user/follow')}>
+            <Text style={{ ...smallTextStyle }}>팔로워</Text>
             <Text fontSize={18}> {currentUser.followers.length}</Text>
           </UserDetail>
-          <UserDetail>
-            <Text style={{ ...smallTextStyle }} onClick={() => navigate('/user/follow')}>
-              {' '}
-              팔로잉
-            </Text>
+          <UserDetail onClick={() => navigate('/user/follow')}>
+            <Text style={{ ...smallTextStyle }}> 팔로잉</Text>
             <Text fontSize={18}> {currentUser.following.length}</Text>
           </UserDetail>
         </UserDetailWrapper>
       </UserInfo>
-
-      {/*  추후 Tab 컴포넌트 교체       
-      <Tab>
-        <Tab.Item onClick={() => console.log('3')}>
-          <Icon
-            name="LIKE_ICON"
-            size={18}
-            onClick={() => setPosts(userPosts)}
-          />
-        </Tab.Item>
-        <Tab.Item onClick={() => console.log('3')}>
-          <Icon
-            name="LIKE_ICON"
-            size={18}
-            onClick={() => setPosts(userPosts)}
-          />
-        </Tab.Item>
-      </Tab>      
+      {/* 
+        //TODO: 추후 기본컴포넌트  Tab을 사용해서 아래 변경 : Icon을 어떻게 처리할지 고민 ,children으로 PostImageContainer를 두는 방식을 현재 사용
       */}
-
       <Tab>
-        <Icon name="LIKE_ICON" size={18} onClick={() => setPosts(userPosts)} />
-        <Icon name="LIKE_ICON" size={18} onClick={() => setPosts(uesrLikePosts)} />
+        <button onClick={() => setPosts(userPosts)}>
+          <Icon name="LIKE_ICON" size={18} />
+        </button>
+        <button onClick={() => setPosts(uesrLikePosts)}>
+          <Icon name="LIKE_ICON" size={18} />
+        </button>
       </Tab>
-      <ImageContainer posts={posts} />
+      <PostImageContainer posts={posts} />
       <Bottom />
     </UserContainter>
   );

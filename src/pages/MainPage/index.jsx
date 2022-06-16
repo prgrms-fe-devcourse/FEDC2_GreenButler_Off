19907a0751b2079f012944df106e8d0f5fdc86d5
gitul0515 <gitul0515@gmail.com>
2022-06-16 17:08:21 +0900

    feat: intersectionObserver가 관찰하는 targetRef 생성

--- src/pages/MainPage/index.jsx
+++ src/pages/MainPage/index.jsx
@@ -2,13 +2,20 @@ import styled from '@emotion/styled';
 import PageWrapper from 'components/basic/pageWrapper';
 import { getPostsPart } from 'utils/apis/postApi';
 import PostItem from './PostItem';
-import { useState, useEffect } from 'react';
+import { useState, useEffect, useRef } from 'react';
 
 const LIMIT = 15;
+
+const observerOption = {
+  root: null,
+  rootMargin: '0px',
+  threshold: 0.9,
+};
 
 const MainPage = () => {
   const [posts, setPosts] = useState([]);
   const [offset, setOffset] = useState(0);
+  const targetRef = useRef(null);
 
   useEffect(() => {
     async function fetchData() {
@@ -21,15 +28,42 @@ const MainPage = () => {
     fetchData();
   }, [offset]);
 
+  useEffect(() => {
+    const observer = new IntersectionObserver((entries) => {
+      if (entries[0].isIntersecting && targetRef.current) {
+        console.log('화면 끝...', entries[0]);
+      }
+    }, observerOption);
+
+    if (targetRef.current) {
+      observer.observe(targetRef.current);
+    }
+    return () => {
+      observer.disconnect();
+    };
+  }, [targetRef]);
+
   return (
     <PageWrapper header nav>
       <PostList>
-        {posts?.map((post) => (
-          <li key={post._id}>
-            <PostItem key={post._id} post={post} />
-          </li>
-        ))}
+        {posts?.map((post, i) => {
+          const isLastItem = posts.length - 1 === i;
+          if (isLastItem) {
+            return (
+              <li key={post._id}>
+                <PostItem key={post._id} post={post} />
+              </li>
+            );
+          } else {
+            return (
+              <li key={post._id}>
+                <PostItem key={post._id} post={post} />
+              </li>
+            );
+          }
+        })}
       </PostList>
+      <div ref={targetRef}></div>
     </PageWrapper>
   );
 };
@@ -37,14 +71,3 @@ const MainPage = () => {
 const PostList = styled.ul``;
 
 export default MainPage;
-
-// export const getPostsPart = ({ offset, limit }) => {
-//   return request({
-//     method: API_METHOD.GET,
-//     url: `/posts/channel/${process.env.REACT_APP_CHANNEL_ID_TOTAL}`,
-//     params: {
-//       offset,
-//       limit,
-//     },
-//   });
-// };

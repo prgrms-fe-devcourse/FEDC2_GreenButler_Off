7f1fd87a97c9af1158a42b0da0c2fc84027f6111
gitul0515 <gitul0515@gmail.com>
2022-06-16 21:26:04 +0900

    refactor: interSectionObserver 수정

--- src/pages/MainPage/index.jsx
+++ src/pages/MainPage/index.jsx
@@ -2,46 +2,57 @@ import styled from '@emotion/styled';
 import PageWrapper from 'components/basic/pageWrapper';
 import { getPostsPart } from 'utils/apis/postApi';
 import PostItem from './PostItem';
-import { useState, useEffect, useRef } from 'react';
+import { useState, useEffect, useCallback, useRef } from 'react';
 
 const LIMIT = 15;
 
-const observerOption = {
-  root: null,
-  rootMargin: '0px',
-  threshold: 0.9,
-};
-
 const MainPage = () => {
   const [posts, setPosts] = useState([]);
+  const [isLoding, setIsLoding] = useState(false);
   const [offset, setOffset] = useState(0);
   const targetRef = useRef(null);
 
   useEffect(() => {
-    async function fetchData() {
-      const initialPosts = await getPostsPart({
+    (async () => {
+      const nextPosts = await getPostsPart({
         offset,
         limit: LIMIT,
       }).then((res) => res.data);
-      setPosts(initialPosts);
-    }
-    fetchData();
-  }, [offset]);
+      setPosts([...posts, ...nextPosts]);
+    })();
+  }, []);
+
+  const onIntersect = useCallback(() => {
+    (async (entries, observer) => {
+      console.log(entries);
+      if (!entries) {
+        return;
+      }
+      if (entries[0].isIntersecting && !isLoding) {
+        observer.unobserve(entries[0].target);
+        setIsLoding(true);
+        setOffset(offset + 5);
+        const nextPosts = await getPostsPart({
+          offset,
+          limit: LIMIT,
+        }).then((res) => res.data);
+        setPosts([...posts, ...nextPosts]);
+        setIsLoding(false);
+        observer.observe(entries[0].target);
+      }
+    })();
+  }, []);
 
   useEffect(() => {
-    const observer = new IntersectionObserver((entries) => {
-      if (entries[0].isIntersecting && targetRef.current) {
-        console.log('화면 끝...', entries[0]);
-      }
-    }, observerOption);
-
+    let observer;
     if (targetRef.current) {
+      observer = new IntersectionObserver(onIntersect, {
+        threshold: 0.5,
+      });
       observer.observe(targetRef.current);
     }
-    return () => {
-      observer.disconnect();
-    };
-  }, [targetRef]);
+    return () => observer && observer.disconnect();
+  }, [targetRef, onIntersect]);
 
   return (
     <PageWrapper header nav>
@@ -71,3 +82,18 @@ const MainPage = () => {
 const PostList = styled.ul``;
 
 export default MainPage;
+
+// useEffect(() => {
+//   const observer = new IntersectionObserver((entries) => {
+//     if (entries[0].isIntersecting && targetRef.current) {
+//       console.log('화면 끝...', entries[0]);
+//     }
+//   }, observerOption);
+
+//   if (targetRef.current) {
+//     observer.observe(targetRef.current);
+//   }
+//   return () => {
+//     observer.disconnect();
+//   };
+// }, [targetRef]);

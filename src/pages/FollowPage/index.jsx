c013f47ff6843f14ef5b66d3ef5090f6d11772c2
bellasimi <gksmf6611@gmail.com>
2022-06-17 21:57:54 +0900

    feat: Follow api 연동

--- src/pages/FollowPage/index.jsx
+++ src/pages/FollowPage/index.jsx
@@ -19,10 +19,64 @@ const FollowPage = () => {
   const [followingUsers, setFollowingUsers] = useState([]); // 얘는 해당 user(id) 유저 정보들의 배열 vs following은 id가 담긴 배열
   const [followerUsers, setFollowerUsers] = useState([]); // 얘는 해당 follower(id) 유저 정보들의 배열 vs followers는 id가 담긴 배열
 
+  useEffect(() => {
+    handleGetUser();
+  }, []);
+
+  //TODO:신영 현재 페이지 유저의 정보 => 전역데이터가 아닌 경우 context를 사용할 수 없어서 직접 api 호출
+  const handleGetUser = useCallback(async () => {
+    if (id) {
+      const { data } = await getUser(id);
+      setUser(data);
+    }
+  }, [id]);
+
+  useEffect(() => {
+    handleGetFollowing();
+    handleGetFollowers();
+    //handleFollowButton();
+  }, [user]);
+
   const [currentTab, setCurrentTab] = useState(FOLLOWER);
   const onActive = (value) => {
     setCurrentTab(value);
   };
+
+  //TODO: FOLLOWING: 내가 팔로잉 한 사람들 user: 그놈들 id , follower: 내 id
+  const handleGetFollowing = useCallback(async () => {
+    const { following } = user;
+    if (following && following.length !== 0) {
+      const data = await Promise.all(
+        following.map((follow) => getUser(follow.user).then((result) => result.data)),
+      );
+      setFollowingUsers(data);
+    }
+  }, [user]);
+
+  //TODO: FOLLOWERS: 나를 팔로잉 한 사람들 user: 내 id , follower: 그 놈들 id
+  const handleGetFollowers = useCallback(async () => {
+    const { followers } = user;
+    if (followers && followers.length !== 0) {
+      const data = await Promise.all(
+        followers.map((follow) => getUser(follow.follower).then((result) => result.data)),
+      );
+      setFollowerUsers(data);
+    }
+  }, [user]);
+
+  /*   const handleFollowButton = useCallback((userId) => {
+    const isFollwing = user.following.some((following) => following.user === userId);
+    setIsFollow((isFollow) => !isFollow);
+    if (isFollow) {
+      console.log('user', user);
+      console.log('userId', userId);
+      onFollow({ userId, followId: id });
+    } else {
+      console.log('user', user);
+      console.log('followId', followId);
+      onUnfollow({ unfollowId: followId });
+    }
+  }, []); */
 
   return (
     <PageWrapper header nav>

2207de76c9f65505f176286e9c1c7d1587d01dbe
jinist94 <325days@naver.com>
2022-06-22 06:30:04 +0900

    remove: 더미데이터 삭제

---
+++ src/hooks/useLocalStorage.js
@@ -0,0 +1,26 @@
+import { useState } from 'react';
+
+const useLocalStorage = (key, initialValue) => {
+  const [storedValue, setStoredValue] = useState(() => {
+    try {
+      const item = localStorage.getItem(key);
+      return item ? JSON.parse(item) : initialValue;
+    } catch (error) {
+      console.error(error);
+      return initialValue;
+    }
+  });
+
+  const setValue = (value) => {
+    try {
+      setStoredValue(value);
+      localStorage.setItem(key, JSON.stringify(value));
+    } catch (error) {
+      console.error(error);
+    }
+  };
+
+  return [storedValue, setValue];
+};
+
+export default useLocalStorage;

98ece87a472f7e05b9882ed2c75c157034578402
gitul0515 <gitul0515@gmail.com>
2022-06-29 00:56:14 +0900

    feat: useSessionStorage 커스텀 훅 추가

---
+++ src/hooks/useSessionStorage.js
@@ -0,0 +1,26 @@
+import { useState } from 'react';
+
+const useSessionStorage = (key, initialValue) => {
+  const [storedValue, setStoredValue] = useState(() => {
+    try {
+      const item = sessionStorage.getItem(key);
+      return item ? JSON.parse(item) : initialValue;
+    } catch (error) {
+      console.error(error);
+      return initialValue;
+    }
+  });
+
+  const setValue = (value) => {
+    try {
+      setStoredValue(value);
+      sessionStorage.setItem(key, JSON.stringify(value));
+    } catch (error) {
+      console.error(error);
+    }
+  };
+
+  return [storedValue, setValue];
+};
+
+export default useSessionStorage;

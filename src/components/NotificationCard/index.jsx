60b62091ee72a13a64018b62bee893c2534bcb1f
yunjjeongjo <two6885@gmail.com>
2022-07-06 04:29:26 +0900

    refactor: NotificationCard, Card 컴포넌트 병합

--- src/components/NotificationCard/index.jsx
+++ src/components/NotificationCard/index.jsx
@@ -1,33 +1,63 @@
 import styled from '@emotion/styled';
 import theme from 'styles/theme';
-import { Card } from 'components';
-import displayedAt from 'utils/functions/displayedAt';
+import { Avatar, Text } from 'components';
+import { IMAGE_URLS } from 'utils/constants/images';
+
+const NotificationText = styled(Text)`
+  margin-bottom: 7px;
+`;
+
+const Container = styled.div`
+  width: 100%;
+  display: flex;
+  align-items: center;
+`;
+
+const Div = styled.div`
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  margin-left: ${({ gap }) => gap};
+`;
+
+const CardWrapper = styled.div`
+  background-color: ${theme.color.mainWhite};
+  width: 100%;
+  height: 129px;
+  border-radius: 15px;
+  padding: 35px 65px 35px 30px;
+  margin-bottom: 16px;
+  display: flex;
+  justify-content: center;
+`;
 
 const NotificationCard = ({
-  notificationId,
-  postId,
-  message,
+  size = 60,
+  gap = '25px',
+  color,
+  fontSize = 18,
   fullName,
-  userId,
-  isSeen,
-  img,
-  createdAt,
+  message,
+  children,
+  img = IMAGE_URLS.PROFILE_IMG,
+  ...props
 }) => {
-  const CardWrapper = styled.div`
-    background-color: ${theme.color.mainWhite};
-    width: 100%;
-    height: 129px;
-    border-radius: 15px;
-    padding: 35px 65px 35px 30px;
-    margin-bottom: 16px;
-    display: flex;
-    justify-content: center;
-  `;
   return (
     <CardWrapper>
-      <Card src={img} fullName={fullName} message={message}>
-        <span>{displayedAt(createdAt)}</span>
-      </Card>
+      <Container {...props}>
+        <Avatar src={img} alt="유저 프로필 이미지" size={size} />
+        <Div gap={gap}>
+          <NotificationText style={{ wordBreak: 'keep-all' }} color={color} lineHeight={'24.52px'}>
+            <Text fontWeight={600} fontSize={fontSize}>
+              {fullName}
+            </Text>
+            <Text fontSize={fontSize}>{message}</Text>
+          </NotificationText>
+          <Text color={theme.color.fontNormal} fontSize={16}>
+            {children}
+          </Text>
+        </Div>
+      </Container>
     </CardWrapper>
   );
 };

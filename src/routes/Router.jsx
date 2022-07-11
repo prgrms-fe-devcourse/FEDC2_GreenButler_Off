9e859b7203751e6730955eb44fa6eb9214b6bfcc
gitul0515 <gitul0515@gmail.com>
2022-07-12 02:41:10 +0900

    feat: PrivateWrapper 컴포넌트 추가 및 적용

--- src/routes/Router.jsx
+++ src/routes/Router.jsx
@@ -1,8 +1,9 @@
 import { useEffect } from 'react';
 import useLocalToken from 'hooks/useLocalToken';
-import { Routes, Route, useNavigate } from 'react-router-dom';
+import { Routes, Route } from 'react-router-dom';
 import { useUserContext } from 'contexts/UserContext';
 import scrollToTop from 'utils/functions/scrollToTop';
+import PrivateWrapper from './PrivateWrapper';
 import {
   LoginPage,
   SignupPage,
@@ -25,24 +26,35 @@ const Router = () => {
   scrollToTop();
 
   useEffect(() => {
-    if (token) {
-      onKeepLoggedIn();
-    }
-  }, []);
+    token && onKeepLoggedIn();
+  }, [token]);
 
   return (
     <Routes>
       <Route path="/login" element={<LoginPage />} />
       <Route path="/signup" element={<SignupPage />} />
       <Route path="/" element={<MainPage />} />
-      <Route path="/post/create" element={<PostEditPage />} />
-      <Route path="/post/edit/:id" element={<PostEditPage />} />
+      <Route element={<PrivateWrapper />}>
+        <Route path="/post/create" element={<PostEditPage />} />
+      </Route>
+      <Route element={<PrivateWrapper />}>
+        <Route path="/post/edit/:id" element={<PostEditPage />} />
+      </Route>
       <Route path="/post/detail/:id" element={<PostDetailPage />} />
       <Route path="/user/:id" element={<UserPage />} />
+      <Route element={<PrivateWrapper />}>
+        <Route path="/user/" element={<UserPage />} />
+      </Route>
       <Route path="/user/follow/:id" element={<FollowPage />} />
-      <Route path="/user/myinfo" element={<MyInfoPage />} />
-      <Route path="/user/myinfo/edit" element={<MyInfoEditPage />} />
-      <Route path="/user/notification" element={<NotificationPage />} />
+      <Route element={<PrivateWrapper />}>
+        <Route path="/user/myinfo" element={<MyInfoPage />} />
+      </Route>
+      <Route element={<PrivateWrapper />}>
+        <Route path="/user/myinfo/edit" element={<MyInfoEditPage />} />
+      </Route>
+      <Route element={<PrivateWrapper />}>
+        <Route path="/user/notification" element={<NotificationPage />} />
+      </Route>
       <Route path="/search" element={<SearchPage />} />
       <Route path="/search/:searchKeyword" element={<SearchPage />} />
       <Route path="/tag/:tag" element={<SearchTagPage />} />
